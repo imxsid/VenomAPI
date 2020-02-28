84792dc93cb8ca587c4fac3438b974d2e60ba9db
DELL <DELL@DESKTOP-ICPNBNP>
2020-02-28 14:41:04 +0530

    Initial Commit

---
+++ src/main/java/io/monty/springbootstarter/venomAPIapp.java
@@ -0,0 +1,13 @@
+package io.monty.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringbootApplication;
+
+@SpringBootApplication
+public class venomAPIapp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(venomAPIapp.class,args);			
+	}
+
+}
